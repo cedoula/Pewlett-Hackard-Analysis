f7534d5537f5c0041db2a83dd5b7c1c81dbab016
cedoula <cedric.lutonda@gmail.com>
2020-09-09 11:07:29 -0500

    Created tailored lists.

--- queries.sql
+++ queries.sql
@@ -34,3 +34,113 @@ WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
 AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
 
 SELECT * FROM retirement_info;
+
+-- Joining departments and dept_manager table
+SELECT d.dept_name,
+	dm.emp_no,
+	dm.from_date,
+	dm.to_date
+FROM departments AS d
+INNER JOIN dept_manager AS dm
+ON d.dept_no = dm.dept_no;
+
+-- Joining retirement_info and dept_emp tables
+SELECT ri.emp_no,
+	ri.first_name,
+	ri.last_name,
+	de.to_date
+INTO current_emp
+FROM retirement_info AS ri
+LEFT JOIN dept_emp AS de
+ON ri.emp_no = de.emp_no
+WHERE de.to_date = '9999-01-01';
+
+-- Employee count by department number
+SELECT COUNT(ce.emp_no), de.dept_no
+INTO retiring_count_per_dept
+FROM current_emp AS ce
+LEFT JOIN dept_emp AS de
+ON ce.emp_no = de.emp_no
+GROUP BY de.dept_no
+ORDER BY de.dept_no;
+
+SELECT * FROM salaries
+ORDER BY to_date DESC;
+
+-- Create table of retiring employees info
+SELECT emp_no, 
+	first_name, 
+	last_name,
+	gender
+INTO emp_info
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+-- Check the table
+SELECT * FROM emp_info;
+
+-- Create emp_info table with full name, gender, salary, to_date
+SELECT e.emp_no,
+	e.first_name,
+	e.last_name,
+	e.gender,
+	s.salary,
+	de.to_date
+INTO emp_info
+FROM employees AS e
+INNER JOIN salaries AS s
+ON e.emp_no = s.emp_no
+INNER JOIN dept_emp as de
+ON e.emp_no = de.emp_no
+WHERE (e.birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (e.hire_date BETWEEN '1985-01-01' AND '1988-12-31')
+AND (de.to_date = '9999-01-01');
+
+DROP TABLE emp_info CASCADE;
+
+-- Create managers list
+SELECT dm.dept_no,
+	d.dept_name,
+	dm.emp_no,
+	ce.last_name,
+	ce.first_name,
+	dm.from_date,
+	dm.to_date
+INTO manager_info
+FROM dept_manager AS dm
+	INNER JOIN departments AS d
+		ON dm.dept_no = d.dept_no
+	INNER JOIN current_emp as ce
+		ON dm.emp_no = ce.emp_no;
+
+-- List of retirees updated
+SELECT ce.emp_no,
+	ce.first_name,
+	ce.last_name,
+	d.dept_name
+INTO dept_info
+FROM current_emp AS ce
+	INNER JOIN dept_emp AS de
+		ON ce.emp_no = de.emp_no
+	INNER JOIN departments AS d
+		ON de.dept_no = d.dept_no;
+
+-- Create retirement_info list for sales team
+SELECT ri.*, d.dept_name
+INTO sales_info
+FROM retirement_info as ri
+	INNER JOIN dept_emp AS de
+		ON ri.emp_no = de.emp_no
+	INNER JOIN departments AS d
+		ON de.dept_no = d.dept_no
+WHERE d.dept_name = 'Sales';
+
+-- Create retirement_info list for sales and development team
+SELECT ri.*, d.dept_name
+INTO sales_dev_info
+FROM retirement_info as ri
+	INNER JOIN dept_emp AS de
+		ON ri.emp_no = de.emp_no
+	INNER JOIN departments AS d
+		ON de.dept_no = d.dept_no
+WHERE d.dept_name IN ('Sales', 'Development');
